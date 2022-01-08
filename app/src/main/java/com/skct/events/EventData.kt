16224b8bd2d5f236471731908773d7b1dd213551
TamilKannanCV <tamilkk100@gmail.com>
2022-01-08 11:49:17 +0530

    updated

--- app/src/main/java/com/skct/events/EventData.kt
+++ app/src/main/java/com/skct/events/EventData.kt
@@ -1,16 +1,28 @@
 package com.skct.events
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 import java.sql.Time
 import java.util.*
 
 @Parcelize
 data class EventData(
+    @SerializedName("name")
     val name: String,
+
+    @SerializedName("description")
     val description: String,
+
+    @SerializedName("venue_data")
     val venueData: String,
+
+    @SerializedName("event_date")
     val eventDate: Date,
+
+    @SerializedName("event_time")
     val eventTime: Time,
+
+    @SerializedName("coordinator_name")
     val coordinatorsName: String
 ) : Parcelable
